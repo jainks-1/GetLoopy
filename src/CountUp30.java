2230790b905e0666065e25b6b57448d0bbaf1ee0
jainks-1 <jainks@mail.uc.edu>
2025-02-27 12:46:52 -0500

    Initial commit

---
+++ src/CountUp30.java
@@ -0,0 +1,17 @@
+public class CountTo30 {
+    public static void main(String[] args) {
+
+        System.out.printf("\nCount from 1 to 30.");
+
+        // count to 30 using for loop
+        for (int count = 0; count <= 30; count++) {
+
+            System.out.println("\ncounter = " + count);
+        }
+
+        // finish and exit
+        System.out.println("\nComplete!");
+        System.exit(0);
+    }
+}
+
---
+++ src/DieRoller.java
@@ -0,0 +1,20 @@
+public class DieRoller {
+    public static void main(String[] args) {
+
+        System.out.print("\nPrinting a cool shape...\n");
+
+        // print the shape using nested for loops
+        for (int count = 1; count <= 5; count++) {
+
+            for (int count2 = 1; count2 <= 5; count2++) {
+                System.out.print("* ");
+            }
+            //create new line for every set of *'s
+            System.out.println();
+        }
+
+        // finish and exit
+        System.out.print("\nComplete!");
+        System.exit(0);
+    }
+}
